d27f2f285fdfbb844f66171b311766fc40afdcba
Jae-Hwan (Jeff)  Jung <j.jung.83@alumni.ubc.ca>
2015-05-28 12:57:26 -0700

    feat: Complete Interactive Resume
    - Build JSONs for data to be inserted into the resume
    - Iterate through JSONs and append to the resume using JQUERY
    - Implement Google Map to show job locations
    - Log click locations using console.log() using JQuery onclick handler

--- js/helper.js
+++ js/helper.js
@@ -24,7 +24,7 @@ var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
@@ -64,11 +64,18 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
+    var name = $('#name').text();
+    var iName = inName(name) || function(){};
     $('#name').html(iName);  
   });
 });
 
+function inName(name) {
+  var names = name.split(' ');
+  var firstName = names[0].slice(0, 1).toUpperCase() + names[0].slice(1).toLowerCase();
+  var lastName = names[1].toUpperCase();
+  return firstName + ' ' + lastName;
+}
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
 */
@@ -85,7 +92,9 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  logClicks(x,y);
 });
 
 
@@ -170,7 +179,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -233,11 +242,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,262 @@
+var bio = {
+    "name": "Jae-Hwan (Jeff) Jung",
+    "role": "Software Developer",
+    "welcomeMessage": "Dear guest, I welcome you to my resume with much love!",
+    "contacts": {
+        "mobile": "604-614-4552",
+        "email": "j.jung.83@alumni.ubc.ca",
+        "github": "midlife83",
+        "twitter": "N/A",
+        "location": "Vancouver, BC V6E 1G1",
+        "blog": "N/A"
+    },
+    "skills": ["C#", "ServiceStack", "WCF", "Silverlight", "Javascript", "CSS", "HTML", "8-hour sleep", "Mental blocking while my girl friend is nagging"],
+    "biopic": "images/MyPic.jpg",
+    "display": function () {
+        function appendFormattedToTopContacts(element, data) {
+            var $topContacts = $("#topContacts");
+            appendFormatted($topContacts, element, data);
+        }
+
+        function appendFormattedToFooterContacts(element, data) {
+            var $footerContacts = $("#footerContacts");
+            appendFormatted($footerContacts, element, data);
+        }
+
+        function displayContactsInHeader() {
+            appendFormattedToTopContacts(HTMLmobile, bio.contacts.mobile);
+            appendFormattedToTopContacts(HTMLemail, bio.contacts.email);
+            appendFormattedToTopContacts(HTMLgithub, bio.contacts.github);
+            appendFormattedToTopContacts(HTMLtwitter, bio.contacts.twitter);
+            appendFormattedToTopContacts(HTMLlocation, bio.contacts.location);
+            appendFormattedToTopContacts(HTMLblog, bio.contacts.blog);
+        }
+
+        function displaySkills() {
+            $header.append(HTMLskillsStart);
+            for (var skill in bio.skills) {
+                if (bio.skills.hasOwnProperty(skill)) {
+                    appendFormatted($("#skills"), HTMLskills, bio.skills[skill]);
+                }
+            }
+        }
+
+        function displayContactsInFooter() {
+            appendFormattedToFooterContacts(HTMLmobile, bio.contacts.mobile);
+            appendFormattedToFooterContacts(HTMLemail, bio.contacts.email);
+            appendFormattedToFooterContacts(HTMLgithub, bio.contacts.github);
+            appendFormattedToFooterContacts(HTMLtwitter, bio.contacts.twitter);
+            appendFormattedToFooterContacts(HTMLlocation, bio.contacts.location);
+        }
+
+        var $header = $("#header");
+        prependFormatted($header, HTMLheaderRole, bio.role);
+        prependFormatted($header, HTMLheaderName, bio.name);
+        displayContactsInHeader();
+        appendFormatted($header, HTMLbioPic, bio.biopic);
+        appendFormatted($header, HTMLWelcomeMsg, bio.welcomeMessage);
+        displaySkills();
+        displayContactsInFooter();
+    }
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "University of British Columbia",
+            "location": "2329 West Mall, Vancouver, BC V6T 1Z4",
+            "degree": "BASc",
+            "majors": ["Computer Engineering - Software Option"],
+            "dates": 2014,
+            "url": "http://www.ubc.ca"
+        },
+        {
+            "name": "British Columbia Institute of Technology",
+            "location": "3800 Cessna Drive, Richmond, BC V7B 0A1",
+            "degree": "Diploma",
+            "majors": ["Aircraft Maintenance Engineering"],
+            "dates": 2007,
+            "url": "http://www.bcit.ca"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Front-End Web Developer Nanodegree",
+            "school": "Udacity",
+            "dates": 2015,
+            "url": "http://www.udacity.com"
+
+        }
+    ],
+    "display": function () {
+        var $education = $('#education');
+
+        function getLastEducationEntry() {
+            return getLastElement("education-entry");
+        }
+
+        function appendFormattedToLastEducationEntry(element, data) {
+            var lastEducationEntry = getLastEducationEntry();
+            appendFormatted(lastEducationEntry, element, data);
+        }
+
+        function displaySchools() {
+            for (var schoolKey in education.schools) {
+                if (education.schools.hasOwnProperty(schoolKey)) {
+                    var school = education.schools[schoolKey];
+                    $education.append(HTMLschoolStart);
+                    var formattedName = formatElement(HTMLschoolName, school.name);
+                    var formattedDegree = formatElement(HTMLschoolDegree, school.degree);
+                    var formattedTitle = formattedName + formattedDegree;
+                    getLastEducationEntry().append(formattedTitle);
+                    appendFormattedToLastEducationEntry(HTMLschoolDates, school.dates);
+                    appendFormattedToLastEducationEntry(HTMLschoolLocation, school.location);
+                    appendFormattedToLastEducationEntry(HTMLschoolMajor, school.majors);
+                }
+            }
+        }
+
+        function displayOnlineCourses() {
+            $education.append(HTMLonlineClasses);
+            for (var onlineCourseKey in education.onlineCourses) {
+                if (education.onlineCourses.hasOwnProperty(onlineCourseKey)) {
+                    $education.append(HTMLschoolStart);
+                    var onlineCourse = education.onlineCourses[onlineCourseKey];
+                    var formattedOnlineTitle = formatElement(HTMLonlineTitle, onlineCourse.title);
+                    var formattedSchool = formatElement(HTMLonlineSchool, onlineCourse.school);
+                    var formattedHeader = formattedOnlineTitle + formattedSchool;
+                    getLastEducationEntry().append(formattedHeader);
+                    appendFormattedToLastEducationEntry(HTMLonlineDates, onlineCourse.dates);
+                    appendFormattedToLastEducationEntry(HTMLonlineURL, onlineCourse.url);
+                }
+            }
+        }
+
+        displaySchools();
+        displayOnlineCourses();
+    }
+};
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Aquatic Informatics",
+            "title": "Software Developer",
+            "location": "Vancouver",
+            "dates": [
+                "2014, May 01 - Current"
+            ],
+            "description": ".NET developer working with various back-end technologies"
+        },
+        {
+            "employer": "Carson Air",
+            "title": "Aircraft Maintenance Engineer",
+            "location": "Vancouver",
+            "dates": [
+                "2007, Jan 01 - 2011, Jan 01"
+            ],
+            "description": "Aircraft Maintenance, repair, inspection"
+        }
+    ],
+    display: function () {
+        var $workExperience = $("#workExperience");
+
+        function getLastWorkEntry() {
+            return getLastElement("work-entry");
+        }
+
+        function appendFormattedToLastWorkEntry(element, data) {
+            var lastWorkEntry = getLastWorkEntry();
+            appendFormatted(lastWorkEntry, element, data);
+        }
+
+        for (var jobKey in work.jobs) {
+            if (work.jobs.hasOwnProperty(jobKey)) {
+                $workExperience.append(HTMLworkStart);
+                var job = work.jobs[jobKey];
+                var formattedEmployer = formatElement(HTMLworkEmployer, job.employer);
+                var formattedTitle = formatElement(HTMLworkTitle, job.title);
+                var formattedEmployerTitle = formattedEmployer + formattedTitle;
+                getLastWorkEntry().append(formattedEmployerTitle);
+                appendFormattedToLastWorkEntry(HTMLworkDates, job.dates[0]);
+                appendFormattedToLastWorkEntry(HTMLworkLocation, job.location);
+                appendFormattedToLastWorkEntry(HTMLworkDescription, job.description);
+            }
+        }
+    }
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "Mockup to Website",
+            "dates": "2015, Jan 25",
+            "description": "A web page created from a given mockup",
+            "images": [
+                "https://placekitten.com/g/250/250",
+                "http://placesheen.com/250/250"
+            ]
+        },
+        {
+            "title": "Amazing Project",
+            "dates": "2013, Feb 12",
+            "description": "Super amazing project",
+            "images": [
+                "https://placekitten.com/g/500/250"
+            ]
+        }
+    ],
+    "display": function () {
+        for (var project in projects.projects) {
+            if (projects.projects.hasOwnProperty(project)) {
+                $("#projects").append(HTMLprojectStart);
+
+                var pro = projects.projects[project];
+                var formattedTitle = formatElement(HTMLprojectTitle, pro.title);
+                var formattedDates = formatElement(HTMLprojectDates, pro.dates);
+                var formattedDescription = formatElement(HTMLprojectDescription, pro.description);
+
+                var projectElement = $(".project-entry:last");
+                projectElement.append(formattedTitle);
+                projectElement.append(formattedDates);
+                projectElement.append(formattedDescription);
+
+                if (pro.images.length > 0) {
+                    for (var image in pro.images) {
+                        if (pro.images.hasOwnProperty(image)) {
+                            var formattedImage = formatElement(HTMLprojectImage, pro.images[image]);
+                            projectElement.append(formattedImage);
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+function formatElement(element, data) {
+    return element.replace("%data%", data);
+}
+
+function appendFormatted(parentElement, childElement, data) {
+    var formattedElement = formatElement(childElement, data);
+    parentElement.append(formattedElement);
+}
+
+function prependFormatted(parentElement, childElement, data) {
+    var formattedElement = formatElement(childElement, data);
+    parentElement.prepend(formattedElement);
+}
+
+function getLastElement(className) {
+    var selector = "." + className + ":last";
+    return $(selector);
+}
+
+bio.display();
+education.display();
+work.display();
+projects.display();
+
+$("#mapDiv").append(googleMap);
+$('#main').append(internationalizeButton);
